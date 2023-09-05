e8b09b2e877fb5c1ef88d6bfd4834d4456a92169
shubhambrandingwaale <shubham.brandingwaale@gmail.com>
2023-09-05 15:57:27 +0530

    mainnew

--- next.config.js
+++ next.config.js
@@ -1,9 +1,31 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
         domains: [ "infrakeysapp.in" ]
-    }
+    },
+
 }
+
+import { withSentryConfig } from "@sentry/nextjs";
+
+const sentryExports = {
+    ...nextConfig,
+
+    sentry {
+        // Sentry config
+    }
+};
+
+const sentryWebpackPluginOptions = {
+    // Sentry Webpack config
+};
+
+if (process.env.ENABLE_SENTRY) console.log("Building frontend with Sentry enabled...");
+
+module.exports = process.env.ENABLE_SENTRY
+    ? withSentryConfig(sentryExports, sentryWebpackPluginOptions)
+    : nextConfig;
 
 module.exports = nextConfig
 
